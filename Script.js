78429aed4762bf29c2061c60d3d297e3de9da4bd
Harry <virtualslide@gmail.com>
2022-08-16 14:42:57 +0100

    added start game functionality to hide the initial splash screen

--- Script.js
+++ Script.js
@@ -1,26 +1,32 @@
+const startButton = document.querySelector('#startGameButton');
+const splashScreen = document.querySelector('#displaySplashInstructionsContainer')
 let avatar = document.getElementById('avatar')
-
-avatar.style.left = '10px'
-let penguinPosition = parseInt(avatar.style.left)
-let lastKeyPressed = ""
 
 function checkKey(lastKeyPressed, currentKeyPressed) {
     return lastKeyPressed !== currentKeyPressed;
 }
 
 function movePenguin(penguinPosition){
-    penguinPosition += 5
+    penguinPosition += 15
     avatar.style.left = penguinPosition + 'px'
     return penguinPosition
 }
 
 function checkEnd(penguinPosition){
-    return penguinPosition > 100;
+    return penguinPosition > 1000;
 }
 
 function endGame(){
-    alert('This is a placeholder message about the game ending')
+    alert('Far out brah! That was totally radical, you found some sweet lines down those faces! Catch you on the flip')
 }
+
+startButton.addEventListener('click', () => {
+    splashScreen.style.display = "none";
+})
+
+avatar.style.left = '0px'
+let penguinPosition = parseInt(avatar.style.left)
+let lastKeyPressed = ""
 
 document.addEventListener("keydown", (event) => {
     switch (event.code) {
