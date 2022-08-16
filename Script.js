cf940787247f92c1eed78b618e62f6598cae883c
Harry <virtualslide@gmail.com>
2022-08-16 13:34:12 +0100

    added movePenguin function

--- Script.js
+++ Script.js
@@ -1,14 +1,22 @@
 let lastKeyPressed = ""
+box2.style.left = '10px'
+let penguinPosition = parseInt(box2.style.left)
 
 function checkKey(lastKeyPressed, currentKeyPressed) {
     return lastKeyPressed !== currentKeyPressed;
+}
+
+function movePenguin(penguinPosition){
+    penguinPosition += 5
+    box2.style.left = penguinPosition + 'px'
+    return penguinPosition
 }
 
 document.addEventListener("keydown", (event) => {
     switch (event.code) {
         case "ArrowLeft":
             if (checkKey(lastKeyPressed, "ArrowLeft")) {
-                // movePenguin()
+                movePenguin(penguinPosition)
                 // console.log to test
                 console.log('penguin moves left')
                 lastKeyPressed = "ArrowLeft"
@@ -16,7 +24,7 @@ document.addEventListener("keydown", (event) => {
             break;
         case "ArrowRight":
             if (checkKey(lastKeyPressed, "ArrowRight")) {
-                // movePenguin()
+                movePenguin(penguinPosition)
                 // console.log to test
                 console.log('penguin moves right')
                 lastKeyPressed = "ArrowRight"
