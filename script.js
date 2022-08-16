602aac8bab5eac6dbe6f950aecad6b5e861a8956
Harry <virtualslide@gmail.com>
2022-08-16 15:28:49 +0100

    trainer review comments incorporated

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const startButton = document.querySelector('#startGameButton');
 const splashScreen = document.querySelector('#displaySplashInstructionsContainer')
-let avatar = document.getElementById('avatar')
+const avatar = document.getElementById('avatar')
+
+avatar.style.left = '0px'
+let penguinPosition = parseInt(avatar.style.left)
+let lastKeyPressed = ""
 
 function checkKey(lastKeyPressed, currentKeyPressed) {
     return lastKeyPressed !== currentKeyPressed;
@@ -22,31 +26,17 @@ function endGame(){
 
 startButton.addEventListener('click', () => {
     splashScreen.style.display = "none";
-})
-
-avatar.style.left = '0px'
-let penguinPosition = parseInt(avatar.style.left)
-let lastKeyPressed = ""
-
-document.addEventListener("keydown", (event) => {
-    switch (event.code) {
-        case "ArrowLeft":
-            if (checkKey(lastKeyPressed, "ArrowLeft")) {
-                lastKeyPressed = "ArrowLeft"
+    document.addEventListener("keydown", (event) => {
+        if(event.code === "ArrowLeft" || event.code === "ArrowRight") {
+            if (checkKey(lastKeyPressed, event.code)) {
+                lastKeyPressed = event.code
                 penguinPosition = movePenguin(penguinPosition)
-                if(checkEnd(penguinPosition)){
+                if (checkEnd(penguinPosition)) {
                     endGame()
                 }
             }
-            break;
-        case "ArrowRight":
-            if (checkKey(lastKeyPressed, "ArrowRight")) {
-                lastKeyPressed = "ArrowRight"
-                penguinPosition = movePenguin(penguinPosition)
-                if(checkEnd(penguinPosition)){
-                    endGame()
-                }
-            }
-            break;
-    }
+        }
+    })
 })
+
+
