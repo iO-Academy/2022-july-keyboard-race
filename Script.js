b515c87b303a4048c38f1ee891a87577624885d6
Harry <virtualslide@gmail.com>
2022-08-16 13:57:34 +0100

    started on some edits

--- Script.js
+++ Script.js
@@ -4,14 +4,6 @@ function checkKey(lastKeyPressed, currentKeyPressed) {
     return lastKeyPressed !== currentKeyPressed;
 }
 
-function checkEnd(penguinPosition){
-    return penguinPosition > 100;
-}
-
-function endGame(){
-    alert('')
-}
-
 document.addEventListener("keydown", (event) => {
     switch (event.code) {
         case "ArrowLeft":
