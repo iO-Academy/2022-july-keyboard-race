2184676a98f5b0ebc8f845b4f9accbba94c33d34
Harry <virtualslide@gmail.com>
2022-08-18 12:31:58 +0100

    minor syntax changes, removed comments and removed duplicate codes

--- leaderboardScript.js
+++ leaderboardScript.js
@@ -1,4 +1,5 @@
 const leaderboardContainer = document.getElementById('mainLeaderboardContainer')
+const mainPageButton = document.getElementById('backButton')
 
 function sortScoresAscending(scoreArray) {
     return [...scoreArray].sort((a, b) => {
@@ -7,17 +8,16 @@ function sortScoresAscending(scoreArray) {
 }
 
 window.addEventListener('load', () => {
-    //create array from localStorage
     const leaderboardArray = JSON.parse(localStorage.getItem('leaderboard'))
-    // sort array by ascending
     const sortedLeaderboardArray = sortScoresAscending(leaderboardArray)
-    //foreach through the array
     sortedLeaderboardArray.forEach((player) => {
-        //get player name and score
         const playerName = player.name
         let playerScore = player.score
         playerScore = parseFloat(playerScore).toFixed(2)
-        //print name and score for each player
         leaderboardContainer.innerHTML += `<div class="leaderboardContent"><p>${playerName}</p><p>${playerScore}s</p></div>`
     })
+})
+
+mainPageButton.addEventListener('click', () => {
+    location.replace('index.html')
 })
--- script.js
+++ script.js
@@ -32,16 +32,12 @@ function saveResult(playerName, playerScore) {
     if (validateName(playerName)) {
         let playerSaveObject = {name: playerName, score: playerScore}
         let leaderboardArray = JSON.parse(localStorage.getItem('leaderboard'))
-        // if previousScoreArray empty create the array and add playerSaveObject
-        // else push playerSaveObject to the end of array
         if (leaderboardArray === null) {
             leaderboardArray = new Array(playerSaveObject)
-            localStorage.setItem('leaderboard', JSON.stringify(leaderboardArray))
-            console.log(leaderboardArray)
         } else {
             leaderboardArray.push(playerSaveObject)
-            localStorage.setItem('leaderboard', JSON.stringify(leaderboardArray))
         }
+        localStorage.setItem('leaderboard', JSON.stringify(leaderboardArray))
         location.replace('leaderboardPage.html')
     } else {
         alert('Please enter a 3 character name else your score won\'t be saved')
